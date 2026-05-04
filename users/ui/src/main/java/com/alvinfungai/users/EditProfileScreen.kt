f038078178589e7a914c74b75821f704f1750a1f
Alvin Fungai <alvin.fungai@gmail.com>
2026-05-04 17:15:38 +0200

    fix manifest build error

--- users/ui/src/main/java/com/alvinfungai/users/EditProfileScreen.kt
+++ users/ui/src/main/java/com/alvinfungai/users/EditProfileScreen.kt
@@ -52,7 +52,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import coil3.compose.AsyncImage
 
 @OptIn(ExperimentalMaterial3Api::class)
